4d813781ecdf9b451d9c646e21dc117cc9f93cfd
Steve Saju <stevesajustephen@gmail.com>
2025-11-02 09:13:43 +0100

    Latest resume added

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
 
               <a
                 target="_blank"
-                href="Resume_SteveSaju.pdf"
+                href="Steve_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
