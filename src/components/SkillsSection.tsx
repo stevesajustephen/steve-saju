c03129d0046bd59e8fb290bb7024e1163ee2174a
Steve Saju <stevesajustephen@gmail.com>
2025-11-04 20:50:21 +0100

    updated skills

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -4,7 +4,7 @@ import { cn } from "../lib/utils";
 const skills = [
   // Frontend
   { name: "React", level: 80, category: "frontend" },
-  { name: "Next.js", level: 8600, category: "frontend" },
+  { name: "Next.js", level: 60, category: "frontend" },
   { name: "TypeScript", level: 95, category: "frontend" },
   { name: "JavaScript", level: 100, category: "frontend" },
   { name: "HTML/CSS", level: 95, category: "frontend" },
